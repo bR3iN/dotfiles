fbf071b6274236ae80642ca818affe8a1ba2c76d
bR3iN <bR3iN@posteo.de>
2021-05-19 09:51:37 +0200

    Small changes

--- hooks/install_deja_vu_fonts.sh
+++ hooks/install_deja_vu_fonts.sh
@@ -6,10 +6,10 @@ TMP="$FONT_PATH/tmp.zip"
 FONT_NAME="DejaVu Fonts"
 URL='https://github.com/dejavu-fonts/dejavu-fonts/releases/download/version_2_37/dejavu-fonts-ttf-2.37.zip'
 
-if ! ls "$FONT_PATH" | egrep -i -q 'Deja.*Vu.*Sans'; then
+if ! ls "$FONT_PATH" | egrep -i -q 'dejavu-fonts'; then
     mkdir -p "$FONT_PATH"
     echo "Downloading $FONT_NAME to $HOME/.local/share/fonts..."
-    wget "$URL" -O "$FONT_PATH/tmp.zip" &>> /dev/null
+    wget "$URL" -O "$TMP" &>> /dev/null
     echo "Extracting fonts..."
     unzip -d "$FONT_PATH" "$TMP" &>> /dev/null
     rm "$TMP"
--- hooks/install_fira_code_nerd_fonts.sh
+++ hooks/install_fira_code_nerd_fonts.sh
@@ -7,7 +7,7 @@ URL='https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.z
 if ! ls "$FONT_PATH" | egrep -q 'Fira.*Code.*Nerd Font'; then
     mkdir -p "$FONT_PATH"
     echo "Downloading $FONT_NAME to $HOME/.local/share/fonts..."
-    wget "$URL" -O "$FONT_PATH/tmp.zip" &>> /dev/null
+    wget "$URL" -O "$TMP" &>> /dev/null
     echo "Extracting fonts..."
     unzip -d "$FONT_PATH" "$TMP" &>> /dev/null
     rm "$TMP"
