f27174f47cc30da89560e4d61e95fcaab5e9591e
bR3iN <bR3iN@posteo.de>
2021-05-21 08:23:02 -0400

    Small changes

---
+++ hooks/install_pop_os_theme.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+set -e
+
+TMP=$(mktemp -d)
+
+install_gtk_theme() {
+    git clone https://github.com/pop-os/gtk-theme "$TMP/pop-gtk"
+    cd "$TMP/pop-gtk"
+    meson build && cd build
+    ninja
+    ninja install
+}
+
+install_icon_theme() {
+    git clone https://github.com/pop-os/icon-theme "$TMP/pop-icons"
+    cd "$TMP/pop-icons"
+    meson build
+    sudo ninja -C "build" install
+}
+
+[ ! -d /usr/share/gnome-shell/theme/Pop ] && install_gtk_theme
+[ ! -d /usr/share/icons/Pop ] && install_icon_theme
