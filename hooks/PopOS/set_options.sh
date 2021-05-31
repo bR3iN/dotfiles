16b5af8386c1399a43fb56753a1a7c347f0d9cc0
bR3iN <bR3iN@posteo.de>
2021-05-31 14:46:57 +0200

    Small changes

--- hooks/PopOS/set_options.sh
+++ hooks/PopOS/set_options.sh
@@ -2,3 +2,4 @@
 
 gsettings set org.gnome.shell.extensions.pop-shell toggle-stacking-global "['<Shift><Super>s']"
 gsettings set org.gnome.shell.extensions.pop-shell activate-launcher "[]"
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'us+intl')]"
--- hooks/gnome/set_options.sh
+++ hooks/gnome/set_options.sh
@@ -5,3 +5,4 @@ gsettings set org.gnome.desktop.input-sources xkb-options "['altwin:prtsc_rwin']
 gsettings set org.gnome.settings-daemon.plugins.media-keys logout "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Super>i']"
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Shift><Super>i']"
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'us+intl')]"
