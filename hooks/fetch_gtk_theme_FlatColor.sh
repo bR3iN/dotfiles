5c86d3f0dbe0cd9cd2bc0cc0f467caea99cf0d54
bR3iN <bR3iN@posteo.de>
2021-07-02 23:28:08 +0200

    Propagate flavours colors to system gtk theme.

--- hooks/fetch_gtk_theme_FlatColor.sh
+++ hooks/fetch_gtk_theme_FlatColor.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/bash
 
 URL="https://github.com/jasperro/FlatColor"
+
 if [ ! -d ~/.themes/FlatColor ]; then
     git clone "$URL" ~/.themes/FlatColor
+fi
+
+if [ ! -d /usr/share/themes/FlatColor ]; then
+    sudo cp -r ~/.themes/FlatColor /usr/share/themes/
+    echo '@import "flavours.css";' | sudo tee -a /usr/share/themes/FlatColor/gtk-3.0/gtk.css  >/dev/null
+    echo '@import "flavours.css";' | sudo tee -a /usr/share/themes/FlatColor/gtk-3.20/gtk.css >/dev/null
 fi
