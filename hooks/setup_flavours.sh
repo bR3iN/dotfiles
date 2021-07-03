47ecb2e2d7ec00b444b89c11f9a224539beeb253
bR3iN <bR3iN@posteo.de>
2021-07-03 12:20:38 +0200

    Created script to setup all things flavours related

---
+++ hooks/setup_flavours.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+# Install flavours and update templates
+cargo install flavours
+~/.cargo/bin/flavours update all
+
+# Make sure all necessary directories exist
+mkdir -p ~/.config/{gtk-2.0,gtk-3.0,gtk-3.20,waybar,sway,kitty,alacritty}
+
+# Make rofi use the flavours infused theme
+mkdir -p ~/.profile.d
+echo 'export ROFI_FLAGS%eq%"$ROFI_FLAGS -theme theme"' > ~/.profile.d/rofi_flavours
+
+# Apply default theme
+~/.cargo/bin/flavours apply nord
