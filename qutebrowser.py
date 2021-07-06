5dc5b2252fbe88dbbf5821645536e3de38f11358
bR3iN <bR3iN@posteo.de>
2021-07-06 19:57:59 +0200

    Allow globally ddg js

--- qutebrowser.py
+++ qutebrowser.py
@@ -5,6 +5,7 @@ c.content.cookies.store = False
 c.content.cookies.store = False
 c.content.geolocation = False
 c.content.javascript.enabled = False
+config.set('content.javascript.images', True, '*://*.duckduckgo.com/*')
 c.content.webgl = False
 c.content.webrtc_ip_handling_policy = 'default-public-interface-only'
 
@@ -15,15 +16,16 @@ c.messages.timeout = 3000
 
 c.editor.command = ['kitty', 'bash', '-c', 'nvim "{file}" "+normal {line}G{column0}l"']
 
-# Keybindings {{{
+## Keybindings
 config.bind('<F1>', 'open -t https://raw.githubusercontent.com/qutebrowser/qutebrowser/master/doc/img/cheatsheet-big.png')
 config.bind('<Space>F', 'spawn firefox -p Hardened {url}')
 config.bind('<Space>f', 'spawn firefox -p Normal {url}')
 # config.bind('<Space>c', 'config-cycle -p content.cookies.accept no-3rdparty never')
 config.bind('<Space>j', 'config-cycle -p content.javascript.enabled')
 config.bind('<Space>m', 'config-cycle -p messages.timeout 0 3000')
-# }}}
 
+
+## Appearance
 c.fonts.default_size = '10pt'
 c.fonts.default_family = "Fira"
 c.tabs.title.format = '{current_title}'
