427a6d02ba99046108708265e8f52cf85e7710cc
bR3iN <bR3iN@posteo.de>
2021-09-25 18:01:30 +0200

    Formatted fish config with fish_indent

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,15 @@
 #!/usr/bin/fish
 
 if [ -x /usr/bin/bat ]
-    alias cat 'bat'
-    set -x PAGER 'bat'
-    set -x BAT_PAGER 'less'
+    alias cat bat
+    set -x PAGER bat
+    set -x BAT_PAGER less
 else if [ -x /usr/bin/batcat ]
-    alias cat 'batcat'
-    set -x PAGER 'batcat' 
-    set -x BAT_PAGER 'less'
+    alias cat batcat
+    set -x PAGER batcat
+    set -x BAT_PAGER less
 else
-    set -x PAGER 'less'
+    set -x PAGER less
 end
 
 if [ -x /usr/bin/exa ]
@@ -26,15 +26,15 @@ alias ga 'git add'
 alias gc 'git commit'
 alias gl 'git log'
 alias gd 'git diff'
-alias se 'sudoedit'
+alias se sudoedit
 
 set fish_greeting
 #set fish_prompt_pwd_dir_length 0
-set -x  GCC_COLORS 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+set -x GCC_COLORS 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 #set -x  EDITOR nvim
 #set -x  VISUAL nvim
-set -x  CDPATH . ~
-set -x  MANPAGER 'nvim +Man!'
+set -x CDPATH . ~
+set -x MANPAGER 'nvim +Man!'
 #set -xa PATH "$HOME/.node_modules/bin"
 #set -xa PATH "$HOME/.local/bin"
 
@@ -45,7 +45,7 @@ if test -f "$HOME/.config/fish/fish.local"
 end
 
 if [ $TERM = xterm-kitty ]
-    alias ssh 'kitty +kitten ssh'
+    alias ssh 'kitty @ set-background-opacity 1; kitty +kitten ssh'
 end
 
 if [ -f /usr/share/fzf/shell/key-bindings.fish ]
