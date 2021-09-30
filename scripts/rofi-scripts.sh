729a4726dafedbcb675f65516d7e7a8ab3f2dcf9
bR3iN <bR3iN@posteo.de>
2021-09-30 10:32:11 +0200

    Removed ROFI_DPI env variable

--- scripts/rofi-scripts.sh
+++ scripts/rofi-scripts.sh
@@ -3,35 +3,30 @@
 CONFIG=~/.config/rofi/scripts.ini
 LOG=/tmp/rofi-scripts.log
 
-main()
-{
-    if [ -z "$1" ]; then
-        echo -en "\0nocustom\x1ftrue\n"
-        awk '/^\[.*\]$/ { gsub(/(\[|\])/,""); print}' "$CONFIG"
-    else
-        declare -a target
-        local path=$(awk -v target="$1" -F "=" '
-        # BEGIN { in_target = 0 }
+if [ -z "$1" ]; then
+    echo -en "\0nocustom\x1ftrue\n"
+    awk '/^\[.*\]$/ { gsub(/(\[|\])/,""); print}' "$CONFIG"
+else
+    declare -a target
+    local path=$(awk -v target="$1" -F "=" '
+    # BEGIN { in_target = 0 }
 
-        /^\[.*\]$/ {
-            if ($0 ~ "^\\["target"\\]$") {
-                in_target = 1
-            } else {
-                in_target = 0
-            }
+    /^\[.*\]$/ {
+        if ($0 ~ "^\\["target"\\]$") {
+            in_target = 1
+        } else {
+            in_target = 0
         }
+    }
 
-        /^path/ && in_target {
-            gsub(/(^ +| +$)/,"", $2)
-            print $2
-            exit
-        }' "$CONFIG")
+    /^path/ && in_target {
+        gsub(/(^ +| +$)/,"", $2)
+        print $2
+        exit
+    }' "$CONFIG")
 
-        if [ -n "$path" ]; then
-            notify-send -t 3000 "$1" "started"
-            eval "(${path})" &> /dev/null &
-        fi
+    if [ -n "$path" ]; then
+        notify-send -t 3000 "$1" "started"
+        eval "(${path})" &> /dev/null &
     fi
-}
-
-main "$@"
+fi
