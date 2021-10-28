bb63d76e9749c111af13679c6250b219aac1bd83
bR3iN <bR3iN@posteo.de>
2021-10-28 20:00:05 +0200

    greetd: fixes on tumbleweed

--- greetd/post-installation.sh
+++ greetd/post-installation.sh
@@ -16,9 +16,13 @@ sed 's/%USER%/'"$INSTALLER_USER"'/g' -i "$GREETD_CONF"
 # Distro specific configuration
 case "$(cat /etc/os-release | grep '^NAME=')" in
     *Tumbleweed*)
+        # Fix "PAM 'greetd' service missing"
+        ln -s /usr/etc/pam.d/greetd /etc/pam.d/greetd
+
         sed 's/user = "greetd"/user = "greeter"/' -i "$GREETD_CONF"
         chown greeter:greeter "$GREETD_CONF"
 
+        # Fix greeter's home directory
         mkdir -p /var/lib/greetd/.config/dconf
         chown -R greeter:greeter /var/lib/greetd
         ;;
