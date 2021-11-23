cd6ff3e04807b0d7ff2fd9ab18c75850bdefcfbe
bR3iN <bR3iN@posteo.de>
2021-11-23 08:25:20 +0100

    Fish: fixed todo function on Silverblue

--- fish/functions/todo.fish
+++ fish/functions/todo.fish
@@ -1,3 +1,7 @@
 function todo
-    nvim /shared/todo.txt
+    if test -f /shared/todo.txt
+        nvim /shared/todo.txt
+    else if test -f /var/shared/todo.txt
+        nvim /var/shared/todo.txt
+    end
 end
