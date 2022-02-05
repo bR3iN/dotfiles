3cf789192ebda4a66adda054b4dc131f10e14100
bR3iN <bR3iN@posteo.de>
2022-02-05 15:17:16 +0100

    Refactored tmux config

--- scripts/is_ssh.sh
+++
@@ -1,6 +0,0 @@
-if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ] \
-    || [ -n "$SSH_CONNECTION" ]; then
-    exit 0
-else
-    exit 1
-fi
--- tmux/battery.sh
+++ tmux/battery.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 BATPATH=/sys/class/power_supply/BAT0
+
+if [ "$1" = '-q' ]; then
+    [ -d "$BATPATH" ]
+    exit "$?"
+fi
+
 STATUS=$(cat "${BATPATH}/status")
 FULL=$(cat "${BATPATH}/energy_full")
 NOW=$(cat "${BATPATH}/energy_now")
 CURRENT=$(( 100 * ${NOW} / ${FULL} ))
 
-echo "${CURRENT} ${STATUS}"
+if [ "$STATUS" == "Charging" ]; then
+    echo " ${CURRENT}%[] "
+else
+    echo " ${CURRENT}% "
+fi
