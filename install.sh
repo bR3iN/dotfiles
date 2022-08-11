c76dac9d5b2a2b1a5e58ccfd29d7384eb2da5ebb
bR3iN <bR3iN@posteo.de>
2022-08-11 20:53:50 +0200

    Rework dotfile installer

--- install.sh
+++ install.sh
@@ -1,29 +1,44 @@
 #!/usr/bin/bash
+
 set -o errexit
 set -o nounset
 set -o pipefail
+
+
+ERROR="\e[1;31m"
+WARNING="\e[1;33m"
+INFO="\e[0;32m"
+CYAN="\e[0;36m"
+BLUE="\e[0;34m"
+PURPLE="\e[0;35m"
+YELLOW="\e[0;33m"
+
 
 export INSTALLER_DIR=$(dirname $(readlink -f "$0"))
 export INSTALLER_USER="$(whoami)"
 
 CONF="${INSTALLER_DIR}/dotfiles.ini"
-OUTPUT_SEPARATOR='='
 
 declare -A INSTALLED
+
 
 function main {
     while [[ "${1-}" =~ ^- ]]; do
         case "${1-}" in
             -c|--copy)
-                NO_SYMLINKS="true"
+                NO_SYMLINKS=true
                 ;;
 
-            -n|--no-deletion) # Do not delete files or directories that aren't symlinks
-                NO_DELETION="true"
+            -d|--dry-run)
+                DRY_RUN=true
+                ;;
+
+            -n|--no-delete) # Do not delete files or directories that aren't symlinks
+                NO_DELETION=true
                 ;;
 
             -y|--always-yes)
-                ALWAYS_YES="true"
+                ALWAYS_YES=true
                 ;;
 
             -l|--list)
@@ -31,27 +46,27 @@ function main {
                 exit
                 ;;
 
-            -h|--help)
-                usage
-                exit
-                ;;
-
-            -f|--file)
-                if [[ -n "$2" ]]; then
+            -f|--config-file)
+                if [[ -n "${2-}" ]]; then
                     CONF="$2"
                     shift
                 else
-                    usage
-                    exit
+                    error "No config file provided."
+                    exit 1
                 fi
+                ;;
+
+            *)
+                error "Unknown option '$1'."
+                exit 1
                 ;;
         esac
         shift
     done
 
     if [[ -z "${1+x}" ]]; then
-        usage
-        exit
+        error "No targets provided."
+        exit 1
     fi
 
     for target in "$@"; do
@@ -60,195 +75,200 @@ function main {
 }
 
 
-function usage {
-    echo "usage... WIP"
-}
-
-
-function listTargets {
-    echo "Available targets:"
-    echo "=================="
-    (awk -f - "$CONF" | sort) << EOF
-    /^\[.*\]/ {
-    gsub(/(\[( |\t)*|( |\t)*\])/,"")
-    print
-}
-EOF
-exit
-}
-
-
 function installTarget {
     local target="$1"
-    if [ -n "${INSTALLED[$target]-}" ]; then
+
+    if [ -z "$target" ]; then
+        return
+    elif [ -n "${INSTALLED[$target]-}" ]; then
+        info "Target $(cwrap "$BLUE" "$target") is already installed. Skipping..."
         return 0
     else
         INSTALLED["$target"]="true"
+        info "Installing target $(cwrap "$BLUE" "$target")."
     fi
 
-    local keys=$(getKeys "$target")
-    if [ -z "$keys" ]; then
-        echo "Section '$target' was not found or contains no keys."
-        return 1
-    fi
+    # Read lines into an array so we don't mess with the stdin inside the loop
+    readarray lines < <(parseTarget "$target")
+    for line in "${lines[@]}"; do
+        IFS=$'\t' read type key value <<< "$line"
 
-    local IFS=$'\n'
-    for key in $keys; do
-        local IFS=' '
-
-        #split `key` into name and value
-        IFS="$OUTPUT_SEPARATOR" read -ra _key <<< "$key"
-        local name="${_key[0]-}"
-        local value="${_key[1]-}"
-
-        if [[ -z "$name" ]] || [[ -z "$value" ]]; then
-            echo "WARNING: '$key' is not a valid key."
-            continue
-        fi
-
-        if [[ ! "$name" =~ ^@ ]]; then
-            createSymlink "$name" "$value"
-        else
-            case "${name#[@]}" in
-                cmd)
-                    runCommand "$value"
-                    ;;
-
-                hook)
-                    runHook "$value"
-                    ;;
-
-                target)
-                    installTarget "$value"
-                    ;;
-                install)
-                    installFile "$value"
-            esac
-        fi
+        case "$type" in
+            links)
+                createSymlink "$key" "$value"
+                ;;
+            hooks)
+                case "$key" in
+                    cmd)
+                        runCommand "$value"
+                        ;;
+                    run)
+                        runHook "$value"
+                        ;;
+                    *)
+                        error "Unknown key '$key'."
+                        exit 1
+                        ;;
+                esac
+                ;;
+            installs)
+                case "$key" in
+                    user)
+                        userInstall "$value"
+                        ;;
+                    system)
+                        systemInstall "$value"
+                        ;;
+                    *)
+                        error "Unknown key '$key'."
+                        exit 1
+                        ;;
+                esac
+                ;;
+            needs)
+                case "$key" in
+                    target)
+                        installTarget "$value"
+                        ;;
+                    *)
+                        error "Unknown key '$key'."
+                        exit 1
+                        ;;
+                esac
+                ;;
+            *)
+                error "Unknown type '$type'."
+                exit 1
+                ;;
+        esac
     done
 }
 
 
+function is_dry_run {
+    [ "${DRY_RUN-}" = true ]
+    return "$?"
+}
+
+
 function runHook {
-    echo "Running hook '$1'"
-    local hook_cmd="${INSTALLER_DIR}/hooks/$1"
-    eval "($hook_cmd)" 2>&1 | sed "s/^/    /" || local exit_code="$?"
-    if [ ! "${exit_code:-0}" -eq 0 ]; then
-        echo "ERROR: Hook '$1' failed with exit code ${exit_code}"
-    fi
+    info "Running hook $(cwrap "$PURPLE" "'$1'")."
+    is_dry_run && return
+    runIndented "${INSTALLER_DIR}/hooks/$1"
 }
 
 
 function runCommand {
-    local command=$(sed 's/%eq%/=/g' <<< "$1")
-    echo "Running command '${command}'"
-    eval "(${command})" 2>&1 | sed 's/^/    /' || local exit_code="$?"
+    info "Running command $(cwrap "$PURPLE" "'$1'")."
+    is_dry_run && return
+    runIndented "$1"
+}
+
+
+function runIndented {
+    bash -c "$1" 2>&1 | indentStdout || local exit_code="$?"
     if [ ! "${exit_code:-0}" -eq 0 ]; then
-        echo "ERROR: Command '${command}' failed with exit code ${exit_code}"
+        error "$(cwrap "$PURPLE" "'$*'") failed with exit code ${exit_code}."
     fi
 }
 
 
-function installFile {
+function indentStdout {
+    sed 's/^/    /' | cpipe "$CYAN"
+}
+
+
+function userInstall {
     local file="$1"
+    info "Installing $(cwrap "$PURPLE" "'$file'")."
+    is_dry_run && return
 
-    if [[ "$file" =~ ^root: ]]; then
-        file="${file#root:}"
-        local as_root=sudo
-    fi
-
-    if [[ "$file" =~ ^lib: ]]; then
-        file="${file#lib:}"
-        local lib=sudo
-    fi
-
-    if [ -f "$INSTALLER_DIR/$file" ]; then
-        if [ -n "${as_root-}" ]; then
-            sudo install -D -m=0755 -t "/usr/local/bin" "$INSTALLER_DIR/$file"
-        elif [ -n "${lib-}" ]; then
-            createSymlink "$file" "$HOME/.local/share/scripts/$(basename "$file")"
-        else
-            createSymlink "$file" "$HOME/.local/bin/$(basename "$file")"
+    local path="$INSTALLER_DIR/$file"
+    if [ -f "$path" ]; then
+        createSymlink "$file" "$HOME/.local/bin/$(basename "$file")"
+    elif [ -d "$path" ]; then
+        (cd "$path"; make install) | indentStdout || local exit_code="$?"
+        if [ ! "${exit_code:-0}" -eq 0 ]; then
+            error "Install directive $(cwrap "$PURPLE" "'$file'") exited with code $?."
         fi
-    elif [ -d "$INSTALLER_DIR/$file" ]; then
-
-        # Make INSTALLER_USER environment variable available if run with sudo
-        if [ -n "${as_root-}" ]; then
-            as_root="$as_root --preserve-env=INSTALLER_USER"
-        fi
-
-        (cd "$INSTALLER_DIR/$file"; ${as_root-} make install) \
-            || local exit_code=$?
     fi
 
-    if [ ! "${exit_code:-0}" -eq 0 ]; then
-        echo "ERROR: install directive $file failed with exit code $exit_code"
+}
+
+
+function systemInstall {
+    local file="$1"
+    info "Installing $(cwrap "$PURPLE" "'$file'")."
+    is_dry_run && return
+
+    local path="$INSTALLER_DIR/$file"
+    if [ -f "$path" ]; then
+        sudo install -D -m=0755 -t "/usr/local/bin" "$path"
+    elif [ -d "$path" ]; then
+        (cd "$path"; sudo --preserve-env=INSTALLER_USER make install) | indentStdout || local exit_code="$?"
+        if [ ! "${exit_code:-0}" -eq 0 ]; then
+            error "Install directive $(cwrap "$PURPLE" "'$file'") exited with code $?."
+        fi
     fi
 }
 
 
 function createSymlink {
     local dest="${INSTALLER_DIR}/$1"
+    local path="$(expandPath "$2")"
+
     if [ ! -e "$dest" ]; then
-        echo "WARNING: Link destination '${dest}' was not found. Skipping..."
+        warning "Link destination '${dest}' was not found. Skipping..."
         return 0
     fi
 
-    if [[ "$2" =~ ^root: ]]; then
-        local as_root=sudo
-        local path=$(getPath "${2#root:}")
+    if [ -z "${NO_SYMLINKS-}" ]; then
+        local cmd=(ln -sT)
+        info "Creating the symlink"
     else
-        local path=$(getPath "$2")
+        local cmd=(cp -rT)
+        info "Creating the copy"
     fi
+    echo -n "    "
+    echo "$(cwrap "$PURPLE" "$path") $(cwrap "$YELLOW" "-->") $(cwrap "$PURPLE" "$dest")."
 
-    if [ -n "${NO_SYMLINKS-}" ] || [ -n "${as_root-}" ]; then
-        local cmd='cp -rT' 
-        local on_success='Created the copy'
-    else
-        local cmd='ln -sT'
-        local on_success='Created the symbolic link'
-    fi
+    is_dry_run && return
 
-    if ${as_root-} [ -h "$path" ]; then
-        ${as_root-} rm -f "$path" || local exit_code=$?
-    elif ${as_root-} [ -e "$path" ]; then
-        confirmAndDelete "$path" ${as_root-} || local exit_code=$?
+    if [ -h "$path" ]; then
+        rm -f "$path"
+    elif [ -e "$path" ]; then
+        confirmAndDelete "$path" || return 0
     fi
 
     if [ "${exit_code:-0}" -eq 0 ]; then
-        ${as_root:+sudo} mkdir -p "$(dirname "$path")"
-        ${as_root:+sudo} $cmd "$dest" "$path"
-
-        if [ "$?" -eq 0 ]; then
-            echo "Created the symbolic link or copy"
-            echo "    $path --> $dest"
-        fi
+        mkdir -p "$(dirname "$path")"
+        "${cmd[@]}" "$dest" "$path"
     fi
 }
 
 
 function confirmAndDelete {
-    # $1 is path of file/dir to remove; $2 is 'sudo' or empty
+    local path="$1"
 
     if [ "${NO_DELETION-}" = true ]; then
-        echo "'$path' is not removed due to '-n|--no-deletion' option"
+        info "$(cwrap "$PURPLE" "'$path'") is not removed due to '-n|--no-deletion' option"
         return 1
     elif [ "${ALWAYS_YES-}" = true ]; then
-        ${2-} rm -rf "$1" && return 0
-        return 1
+        rm -rf "$path"
+        return
     fi
 
-    echo "WARNING: The file/directory"
+    warning "The file/directory"
     echo
-    echo "  '$1'"
+    echo "    $(cwrap "$PURPLE" "'$1'")"
     echo
     echo -n "already exists. Do you want to delete it? (y|N) "
 
     while true; do
         read confirm
         case "$confirm" in
-            y|Y) ${2-} rm -rf "$1" && return 0
-                return 1
+            y|Y) rm -rf "$path"
+                return
                 ;;
             n|N)
                 return 1
@@ -257,14 +277,14 @@ function confirmAndDelete {
                 return 1
                 ;;
             *)
-                echo "Please anwser 'y' or 'n'."
+                echo -n "Please anwser 'y' or 'n'. "
                 ;;
         esac
     done
 }
 
 
-function getPath {
+function expandPath {
     if [[ "$1" =~ ^~/ ]]; then
         echo "$HOME/${1#"~/"}"
     elif [[ ! "$1" =~ ^/ ]]; then
@@ -275,37 +295,118 @@ function getPath {
 }
 
 
-function getKeys {
-    awk -F= -v TARGET_SECTION="$1" \
-        -f - "$CONF" << EOF
-            BEGIN {
-            in_target_section = 0
-            ws = "( |\t)*"
-        }
+function listTargets {
+    echo "Available targets:"
+    echo "=================="
+    awk "$AWK_LIST_TARGETS" "$CONF" | sort
+    exit
+}
 
-# Matches non-target header
-/^\[.*\]/ \
-    && \$0 !~ "^\\\\[" ws TARGET_SECTION ws "\\\\]" \
-    {
-        in_target_section = 0
-    }
 
-in_target_section \
-    && \$0 !~ "^" ws ";" \
-    && \$0 ~ ws "[^ \t]" \
-    {
-        gsub(/(^(\t| )+|(\t| )+\$)/,"",\$1)
-        gsub(/(^(\t| )+|(\t| )+\$)/,"",\$2)
-        print \$1 "$OUTPUT_SEPARATOR" \$2
-    }
+function parseTarget {
+    awk "$AWK_PARSE_TARGET" OFS='\t' TARGET="$1" "$CONF"
+}
 
-# Matches target header
-\$0 ~ "^\\\\[" ws TARGET_SECTION ws"\\\\]" \
-    {
-        in_target_section = 1
-    }
-EOF
+
+AWK_COMMON='
+BEGIN {
+    header_left  = "^\\s*\\["
+    header_right = "\\]\\s*$"
+    header_tail = "\\.(links|hooks|installs|needs)"
+    valid_target = "[a-zA-Z0-9\\-_]+"
+}'
+
+
+AWK_LIST_TARGETS='
+'"$AWK_COMMON"'
+
+$0 ~ header_left valid_target header_tail header_right {
+    target = $0
+    sub(header_left, "", target)
+    sub(header_right, "", target)
+    sub(header_tail, "", target)
+    targets[target]
+}
+
+END { for (target in targets) { print target } }'
+
+
+AWK_PARSE_TARGET='
+'"$AWK_COMMON"'
+
+BEGIN { comment_or_empty = "^\\s*(;|\\s*$)" }
+
+$0 ~ header_left valid_target header_tail header_right {
+    header = parse_header($0)
+    in_target = header ~ TARGET header_tail
+    TYPE = parse_type(header)
+}
+
+in_target && $0 !~ header_left && $0 !~ comment_or_empty {
+    print TYPE, parse_key($0), parse_value($0)
+}
+
+function trim(s) {
+    sub(/^\s*/, "", s)
+    sub(/\s*$/, "", s)
+    return s
+}
+
+function parse_key(line) {
+    sub(/=.*$/, "", line)
+    return trim(line)
+}
+
+function parse_value(line) {
+    sub(/^[^=]*=/, "", line)
+    return trim(line)
+}
+
+function parse_type(header) {
+    sub(valid_target "\\.", "", header)
+    return header
+}
+
+function parse_header(line) {
+    sub(header_left,  "", line)
+    sub(header_right, "", line)
+    return line
+}'
+
+
+function cwrap {
+    echo -ne "$1$2\e[0m"
+}
+
+
+function cpipe {
+    echo -ne "$1"
+    cat
+    echo -ne "\e[0m"
+}
+
+
+function clog {
+    echo -n "$(cwrap "$1" "$2: ")"
+    shift; shift
+    echo -e "$@"
+}
+
+
+function error {
+    clog "$ERROR" "Error" "$@"
+}
+
+
+function warning {
+    clog "$WARNING" "Warning" "$@"
+}
+
+
+function info {
+    clog "$INFO" "Info" "$@"
 }
 
 
 main "$@"
+# main Bash Desktop Bash
