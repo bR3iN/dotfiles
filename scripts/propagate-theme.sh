f7ec9980793249ba01dd9e41ef108c4c86a89bfa
bR3iN <bR3iN@posteo.de>
2023-04-29 14:20:16 +0200

    Reintegrate Greetd

--- scripts/propagate-theme.sh
+++ scripts/propagate-theme.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 pkexec bash <<< "
 if [ -d /var/lib/greetd ]; then
-    mkdir -p /var/lib/greetd/.config/gtk-3.0
-    cp \"$HOME/.config/gtk-3.0/gtk.css\" /var/lib/greetd/.config/gtk-3.0/
+    for dir in gtk-3.0 gtk-4.0; do
+        mkdir -p /var/lib/greetd/.config/\$dir
+        cp \"$HOME/.config/\$dir/gtk.css\" /var/lib/greetd/.config/\$dir/
+    done
 fi
+
 for wallpaper in current current_blurred; do
     if [ -f \"$HOME/Wallpaper/\$wallpaper\" ]; then
         cp \"$HOME/Wallpaper/\$wallpaper\" /usr/share/backgrounds/
