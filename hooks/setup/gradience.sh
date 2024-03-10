0de3cf22ac852ae11f59e67f0fbd3685feef4f9c
bR3iN <bR3iN@posteo.de>
2024-03-10 13:31:22 +0100

    (Try to) fix gradience flatpak setup

--- hooks/setup/gradience.sh
+++ hooks/setup/gradience.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+set -e
+
 if ! which flatpak &>/dev/null; then
     echo "ERROR: Flatpak not installed"
     exit 1
@@ -18,23 +20,32 @@ fi
 # Install adw-gtk3
 sudo flatpak install -y org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
 
-if [ -d "/usr/share/themes/adw-gtk3" ]; then
+if [ -d "/usr/share/themes/adw-gtk3" ] && [ -d "$HOME/.themes/adw-gtk3" ]; then
     echo "adw-gtk3 is already installed"
 else
     TMP_PATH=$(mktemp -u)
     wget -O "$TMP_PATH" "https://github.com/lassekongo83/adw-gtk3/releases/download/v4.5/adw-gtk3v4-5.tar.xz"
-    sudo mkdir -p /usr/share/themes
-    sudo tar xaf "$TMP_PATH" --directory=/usr/share/themes
+
+    if [ ! -d "/usr/share/themes/adw-gtk3" ]; then
+        sudo mkdir -p /usr/share/themes
+        sudo tar xaf "$TMP_PATH" --directory=/usr/share/themes
+    fi
+    # Flatpak can't expose /usr/share/themes and flatpak theme doesn't work that well (TODO), so install also into ~/.themes
+    if [ -d "$HOME/.themes/adw-gtk3" ]; then
+        mkdir -p "$HOME/.themes"
+        tar xaf "$TMP_PATH" --directory="$HOME/.themes"
+    fi
     rm "$TMP_PATH"
 fi
 
-# Set adw-gtk3 as theme
+# Set adw-gtk3 as theme for the user
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
 
 # Gradience doesn't create those automatically
 mkdir -p ~/.config/gtk-{4,3}.0
 
 # Allow flatpak apps to read color overwrites and in particular Gradience to write them
+sudo flatpak override --filesystem=home/.themes
 sudo flatpak override --filesystem=xdg-config/gtk-4.0
 sudo flatpak override --filesystem=xdg-config/gtk-3.0
 sudo flatpak override --env=GTK_THEME=adw-gtk3
