5116b05264de507b090de76f75c8f22dc55efbab
bR3iN <bR3iN@posteo.de>
2025-10-13 09:53:17 +0200

    disable kitty opacity

--- fish/conf.d/venv.fish
+++ fish/conf.d/venv.fish
@@ -1,6 +1,6 @@
 # Don'generate an extra prompt or Python venvs; we do this ourself in ./functions/fish_prompt
-set -x VIRTUAL_ENV_DISABLE_PROMPT yes
-set -l default_venv ~/.local/state/venv/default/bin/activate.fish
-if test -f $default_venv
-    source $default_venv
-end
+# set -x VIRTUAL_ENV_DISABLE_PROMPT yes
+# set -l default_venv ~/.local/state/venv/default/bin/activate.fish
+# if test -f $default_venv
+#     source $default_venv
+# end
